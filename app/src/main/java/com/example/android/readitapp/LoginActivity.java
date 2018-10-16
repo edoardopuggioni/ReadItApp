82414a8fa9ed89c96cba62a6a1a3abf2a9f5763c
Andreas Koj <kojandreas@gmail.com>
2018-10-16 12:59:33 +0200

    -activity_login layout skeleton completed -deleted action bar from activity_login -added default picture -changed basic color values colors.xml

--- app/src/main/java/com/example/android/readitapp/LoginActivity.java
+++ app/src/main/java/com/example/android/readitapp/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.readitapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity
 {
@@ -11,6 +13,11 @@ public class LoginActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //test
+
+        //hiding the action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
     }
 }
