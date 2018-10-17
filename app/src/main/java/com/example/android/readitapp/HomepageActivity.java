98e0fc9402f305c7c9330e9b0ed28258f6a3e74d
Edoardo Puggioni <puggioniedoardo@gmail.com>
2018-10-17 10:54:58 +0200

    Prepared skeleton for navigation from Homepage activity.

--- app/src/main/java/com/example/android/readitapp/HomepageActivity.java
+++ app/src/main/java/com/example/android/readitapp/HomepageActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.readitapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomepageActivity extends AppCompatActivity
 {
@@ -10,5 +12,23 @@ public class HomepageActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+    }
+
+    public void goToActivity(View view)
+    {
+        // Do something in response to button
+        switch (view.getId())
+        {
+            case R.id.homepage_quiz_button:
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.homepage_achievements_button:
+                break;
+
+            case R.id.homepage_challenges_button:
+                break;
+        }
     }
 }
