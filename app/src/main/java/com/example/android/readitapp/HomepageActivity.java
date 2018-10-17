538024d398c15e09cc5ad43c06af22dcc99c6557
Edoardo Puggioni <puggioniedoardo@gmail.com>
2018-10-17 15:13:34 +0200

    Added welcome message in Homepage to test the currentUser object.

--- app/src/main/java/com/example/android/readitapp/HomepageActivity.java
+++ app/src/main/java/com/example/android/readitapp/HomepageActivity.java
@@ -4,14 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class HomepageActivity extends AppCompatActivity
 {
+    TextView welcomeMessagge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        AppManager appManager = AppManager.getInstance();
+        User currentUser = appManager.getCurrentUser();
+
+        welcomeMessagge = findViewById(R.id.homepage_welcome);
+        welcomeMessagge.setText("Welcome " + currentUser.getFirstName() + "!");
     }
 
     public void goToActivity(View view)
