0720dd1673f140924d3e3cd9d0679519801c155b
Edoardo Puggioni <puggioniedoardo@gmail.com>
2018-10-18 17:35:20 +0200

    Fixed fake data and constructors.

--- app/src/main/java/com/example/android/readitapp/Book.java
+++ app/src/main/java/com/example/android/readitapp/Book.java
@@ -7,25 +7,20 @@ public class Book
     private AgeGroupsEnum ageGroup;
     private String summary;
     private String cover;
-    // I commented out Quiz for debugging purposes and to create books without quizzes
-   // private Quiz quiz;
+    private Quiz quiz;
 
-    public Book (String title){
-        this.title=title;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-
-
-    // This constructor does not contain quiz object!
-    public Book(String isbn, String title, AgeGroupsEnum ageGroup, String summary, String cover) {
+    public Book( String isbn, String title, AgeGroupsEnum ageGroup, String summary, String cover )
+    {
         this.isbn = isbn;
         this.title = title;
         this.ageGroup = ageGroup;
         this.summary = summary;
         this.cover = cover;
     }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
 }
--- app/src/main/java/com/example/android/readitapp/FakeData.java
+++ app/src/main/java/com/example/android/readitapp/FakeData.java
@@ -13,8 +13,8 @@ public class FakeData
     ArrayList<User> users;
     ArrayList<Challenge> challenges;
     ArrayList<ChallengeParticipation> challengeParticipations;
-
-    ArrayList<Book> books;
+    ArrayList<Book> borrowedBooks;
+    ArrayList<Book> returnedBooks;
 
     private static final FakeData ourInstance = new FakeData();
 
@@ -34,8 +34,8 @@ public class FakeData
         Achievements achievements;
         User user;
         ChallengeParticipation challengeParticipation;
+        ArrayList<Book> books;
         int gems;
-        ArrayList<Book> borrowedBooks;
 
         books = new ArrayList<>();
         challenges = new ArrayList<>();
@@ -46,57 +46,66 @@ public class FakeData
 
         // initializing books
         book = new Book("9780747532743",
-                            "Harry Potter and the Philisopher's Stone",
-                            AgeGroupsEnum.DRAGONS,
-                            "Harry Potter is an ordinary boy who lives in a cupboard under the stairs at his Aunt Petunia and Uncle Vernon's house, which he thinks is normal for someone like him who's parents have been killed in a 'car crash'. He is bullied by them and his fat, spoilt cousin Dudley, and lives a very unremarkable life with only the odd hiccup (like his hair growing back overnight!) to cause him much to think about. That is until an owl turns up with a letter addressed to Harry and all hell breaks loose! He is literally rescued by a world where nothing is as it seems and magic lessons are the order of the day. Read and find out how Harry discovers his true heritage at Hogwarts School of Wizardry and Witchcraft, the reason behind his parents mysterious death, who is out to kill him, and how he uncovers the most amazing secret of all time, the fabled Philosopher's Stone! All this and muggles too. Now, what are they?",
-                            "No image");
+                "Harry Potter and the Philisopher's Stone",
+                AgeGroupsEnum.DRAGONS,
+                "Harry Potter is an ordinary boy who lives in a cupboard under the stairs at his Aunt Petunia and Uncle Vernon's house, which he thinks is normal for someone like him who's parents have been killed in a 'car crash'. He is bullied by them and his fat, spoilt cousin Dudley, and lives a very unremarkable life with only the odd hiccup (like his hair growing back overnight!) to cause him much to think about. That is until an owl turns up with a letter addressed to Harry and all hell breaks loose! He is literally rescued by a world where nothing is as it seems and magic lessons are the order of the day. Read and find out how Harry discovers his true heritage at Hogwarts School of Wizardry and Witchcraft, the reason behind his parents mysterious death, who is out to kill him, and how he uncovers the most amazing secret of all time, the fabled Philosopher's Stone! All this and muggles too. Now, what are they?",
+                "No image");
         books.add(book);
 
-        book = new Book( "978074232743","Harry Potter and the Chamber of Secrets",
-                            AgeGroupsEnum.DRAGONS,
-                            "Harry Potter’s summer has included the worst birthday ever, doomy warnings from a house-elf called Dobby, and rescue from the Dursleys by his friend Ron Weasley in a magical flying car! Back at Hogwarts School of Witchcraft And Wizardry for his second year, Harry hears strange whispers echo through empty corridors – and then the attacks start. Students are found as though turned to stone … Dobby’s sinister predictions seem to be coming true. ",
-                            "No image");
+        book = new Book("978074232743", "Harry Potter and the Chamber of Secrets",
+                AgeGroupsEnum.DRAGONS,
+                "Harry Potter’s summer has included the worst birthday ever, doomy warnings from a house-elf called Dobby, and rescue from the Dursleys by his friend Ron Weasley in a magical flying car! Back at Hogwarts School of Witchcraft And Wizardry for his second year, Harry hears strange whispers echo through empty corridors – and then the attacks start. Students are found as though turned to stone … Dobby’s sinister predictions seem to be coming true. ",
+                "No image");
         books.add(book);
 
 
         // initializing challenges
         // create an array list of books for specific challenge, 
-        challenge = new Challenge("Harry Potter Challenge", AgeGroupsEnum.DRAGONS, new Date(2018,10,20), new Date(2018, 12,15),books,"Info is missing");
-
+        challenge = new Challenge("Harry Potter Challenge", AgeGroupsEnum.DRAGONS,
+                new Date(2018, 10, 20), new Date(2018, 12, 15),
+                books, "Info is missing");
         challenges.add(challenge);
 
-        // challenge participation
-
-
-
-
+        // Init users list.
         users = new ArrayList<>();
-        //changed users1 pass to 1 ""
+
+
+        // Init user 1.
+
         pswHash = appManager.md5("1");
         achievements = new Achievements();
+
         challengeParticipation = new ChallengeParticipation(challenges.get(0), 100);
-        challengeParticipations.add(0, challengeParticipation);
-        borrowedBooks = new ArrayList<Book>(Collections.singleton(new Book("Harry Potter1")));
-        user = new User( 1, pswHash, "Edoardo", AgeGroupsEnum.DRAGONS, achievements, 50, borrowedBooks, challengeParticipations);
+        challengeParticipations.add(challengeParticipation);
+
+        borrowedBooks = new ArrayList<>();
+        borrowedBooks.add(books.get(0));
+
+        returnedBooks = new ArrayList<>();
+        returnedBooks.add(books.get(1));
+
+        user = new User(1, pswHash, "Edoardo", AgeGroupsEnum.DRAGONS,
+                achievements, 50, borrowedBooks, returnedBooks, challengeParticipations);
         users.add(user);
+
+
+        // Init user 2.
 
         pswHash = appManager.md5("p2");
         achievements = new Achievements();
-        challengeParticipation = new ChallengeParticipation();
-        borrowedBooks = new ArrayList<Book>(Collections.singleton(new Book("Harry Potter2")));
-        user = new User( 2, pswHash, "Alessandro", AgeGroupsEnum.TIGERS, achievements, 173, borrowedBooks, challengeParticipations );
+
+        challengeParticipation = new ChallengeParticipation(challenges.get(0), 100);
+        challengeParticipations.add(challengeParticipation);
+
+        borrowedBooks = new ArrayList<>();
+        borrowedBooks.add(books.get(0));
+
+        returnedBooks = new ArrayList<>();
+        returnedBooks.add(books.get(1));
+
+        user = new User(2, pswHash, "Alessandro", AgeGroupsEnum.TIGERS,
+                achievements, 173, borrowedBooks, returnedBooks, challengeParticipations);
         users.add(user);
-
-        pswHash = appManager.md5("p3");
-        achievements = new Achievements();
-        challengeParticipation = new ChallengeParticipation();
-        borrowedBooks = new ArrayList<Book>(Collections.singleton(new Book("Harry Potter3")));
-        user = new User( 3, pswHash, "Andreas", AgeGroupsEnum.BUNNIES, achievements, 4, borrowedBooks, challengeParticipations);
-        users.add(user);
-
-
-
-
     }
 
 
