4c49a1a6d7ce24027239c220d66e4f9c91b8978b
Andreas Koj <kojandreas@gmail.com>
2018-10-24 14:31:28 +0200

    updated challenges

--- app/src/main/java/com/example/android/readitapp/CustomArrayAdapterChallengesIn.java
+++ app/src/main/java/com/example/android/readitapp/CustomArrayAdapterChallengesIn.java
@@ -1,7 +1,6 @@
 package com.example.android.readitapp;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class CustomArrayAdapterChallengesIn extends ArrayAdapter
 {
@@ -27,22 +27,26 @@ public class CustomArrayAdapterChallengesIn extends ArrayAdapter
     public View getView (int position, View convertView, ViewGroup parent) {
 
         appManager = AppManager.getInstance();
-
+        ChallengeParticipation challengeParticipationReference = appManager.getCurrentUser().getChallengesParticipations().get(position);
 
         //challengeParticipation challengeParticipation = (ChallengeParticipation) getItem(position);
         if (convertView == null)
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_challenges_in_custom_item,
                     parent, false);
 
+        Date currentDate = new Date();
+        //get
+
         //handlers to the elements in activity .xml
+
         TextView list_item_challenge_title = (TextView) convertView.findViewById(R.id.challenge_title);
-        TextView list_item_challenge_days_to_end_ = (TextView) convertView.findViewById(R.id.challenge_days_to_end_value);
+        TextView list_item_challenge_gems_ = (TextView) convertView.findViewById(R.id.challenge_gems);
+        TextView list_item_challenge_days_to_end = (TextView) convertView.findViewById(R.id.days_to_end_value);
 
-        list_item_challenge_title.setText(appManager.getCurrentUser().getChallengesParticipations().get(position).getChallenge().getTitle());
-        list_item_challenge_days_to_end_.setText("" +appManager.getCurrentUser().getChallengesParticipations().get(position).getGems());
-
+        list_item_challenge_title.setText(challengeParticipationReference.getChallenge().getTitle());
+        list_item_challenge_gems_.setText("" + challengeParticipationReference.getGems());
+        //list_item_challenge_days_to_end.setText("" + challengeParticipationReference.getGems());
 
         return convertView;
     }
-
 }
